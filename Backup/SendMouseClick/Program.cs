5f4e24d872b27dfc4cb6985bd4d1d65df1c39105
Steve Lamb <steve@thoughtfort.com>
2012-04-07 14:15:23 -0700

    initial checkin

---
+++ Backup/SendMouseClick/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace SendMouseClick
+{
+    class Program
+    {
+        [DllImport("user32")]
+        public static extern int SetCursorPos(int x, int y);
+
+        [DllImport("user32.dll")]
+        public static extern void mouse_event(int dwFlags, int dx, int dy, int cButtons, int dwExtraInfo);
+
+        public enum MouseEventType : int
+        {
+            LeftDown = 0x02,
+            LeftUp = 0x04,
+            RightDown = 0x08,
+            RightUp = 0x10
+        }
+
+
+        static void Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                while (true)
+                {
+                    Console.WriteLine("Cursor pos: " + Cursor.Position.X + ", " + Cursor.Position.Y);
+                    Thread.Sleep(50);
+                }
+            }
+
+            int x = Convert.ToInt32(args[0]);
+            int y = Convert.ToInt32(args[1]);
+
+            while (true)
+            {
+                Console.WriteLine("Sending Click: " + DateTime.Now.ToShortTimeString());
+                SetCursorPos(x, y);
+                mouse_event((int)MouseEventType.LeftDown, x, y, 0, 0);
+                mouse_event((int)MouseEventType.LeftUp, x, y, 0, 0);
+
+                Thread.Sleep(30000);
+            }
+        }
+    }
+}
---
+++ Backup/SendMouseClick/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("SendMouseClick")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SendMouseClick")]
+[assembly: AssemblyCopyright("Copyright ©  2008")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("d985f0da-367e-4949-a53e-3799951d4a83")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
